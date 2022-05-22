9f5f67dd67413eaae5e1e880523f09da451ceaad
Iain Simmons <isimmons@squiz.net>
2022-05-23 09:08:17 +1000

    Update readme, license, etc

--- config/setupTests.js
+++
@@ -1,7 +0,0 @@
-import 'regenerator-runtime/runtime'
-import '@testing-library/jest-dom'
-
-import preact from 'preact'
-import * as hook from 'preact/hooks'
-global.preact = preact
-global._hooks = hook
